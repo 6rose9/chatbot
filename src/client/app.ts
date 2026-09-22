2d95b7522b8f1ff0d9af0a3af3c9b05443081751
6rose9 <zinzinthinzaw8@gmail.com>
2026-09-22 10:00:26 +0630

    chatbot: client app

--- src/client/app.ts
+++ src/client/app.ts
@@ -7,12 +7,15 @@ const displaybox = document.getElementById("displaybox") as HTMLDivElement;
 const clearhistory = document.getElementById("clear-history") as HTMLAnchorElement;
 const spinner = document.getElementById("loading-spinner") as HTMLDivElement;
 
+let lastMessageDiv: HTMLDivElement | null = null;
+let isNewInput: boolean = true;
+
 // for localhost => ws://localhost:8000/ws
 // for cloud => wss://${window.location.hostname}/ws
 
-const wsURL = (path:string) => {
+const wsURL = (path: string) => {
     const islocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-    if(islocal) return `ws://localhost:9000${path}`;
+    if (islocal) return `ws://localhost:9000${path}`;
     return `${window.location.protocol === "https:" ? "wss" : "ws"}://${window.location.hostname}${path}`
 }
 
@@ -31,4 +34,34 @@ ws.onclose = (event) => {
     spinner.style.display = "none";
 }
 
-ws.onmessage = () => {}
+ws.onmessage = () => { }
+
+sendbtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const input = userinput.value.trim();
+    if (!input) return;
+
+    if (displaybox.querySelector("small")) displaybox.innerHTML = "";
+
+    // create ui
+    const userDiv = document.createElement("div");
+    userDiv.classList = "p-3 ms-3 chat-message user-input";
+    userDiv.textContent = input;
+    displaybox.appendChild(userDiv);
+
+    // send websocket & save to localStorage
+    ws.send(input);
+    saveToLocal("user-input", input);
+
+    userinput.value = "";
+    userinput.focus();
+
+    lastMessageDiv = null;
+    isNewInput = true;
+    spinner.style.display = "block";
+});
+
+function saveToLocal(role: string, content: string) {
+
+}
